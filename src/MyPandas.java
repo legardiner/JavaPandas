5c72bcdf45f29c9b0f00c43de8d6a5b1be2e2138
legardiner <legardiner@mail.lipscomb.edu>
2017-11-26 21:45:10 -0600

    Rename files

--- src/MyPandas.java
+++ src/MyPandas.java
@@ -1,74 +1,22 @@
+import java.util.ArrayList;
 
-public class MyPandas {
-	public MyDataFrame head(int n) {
-		return rows;
-	}
-	
-	public MyDataFrame tail(int n) {
-		return rows;
-	}
-	
-	public String dType(int index) {
-		return type;
-	}
-	
-	public String dType(String name) {
-		return type;
-	}
-	
-	public MyDataFrame slice(int index) {
-		return column;
-	}
-	
-	public MyDataFrame slice(String name) {
-		return column;
-	}
-	
-	public MyDataFrame slice(int[] indexArr) {
-		return columns;
-	}
-	
-	public MyDataFrame filter(String col, String op, Object o) {
-		return filteredData;
-	}
-	
-	public MyDataFrame loc(int index) {
-		return rows;
-	}
-	
-	public MyDataFrame loc(String label) {
-		return rows;
-	}
-	
-	public MyDataFrame loc(int from, int to) {
-		return rows;
-	}
-	
-	public MyDataFrame loc(String from, String to) {
-		return rows;
-	}
-	
-	public MyDataFrame sort(int index) {
-		return sortedData;
-	}
-	
-	public MyDataFrame sort(String name) {
-		return sortedData;
-	}
-	
-	public Object getMin(int index) {
-		return min;
-	}
-	
-	public Object getMin(String label) {
-		return min;
-	}
-	
-	public Object getMax(int index) {
-		return max;
-	}
-	
-	public Object getMax(String label) {
-		return max;
-	}
+public class MyDataFrame {
+    public ArrayList<String> state; 
+    public ArrayList<String> gender;
+    public ArrayList<Integer> year;
+    public ArrayList<String> name;
+    public ArrayList<Integer> count;
+    
+    public MyDataFrame readCSV(String path) {
+        return type;
+    }
+    
+    public void writeCSV(MyDataFrame data, String path) {
+        
+    }
+    
+    public MyDataFrame concat(MyDataFrame df1, MyDataFrame df2) {
+        return type; 
+    }
+
 }
