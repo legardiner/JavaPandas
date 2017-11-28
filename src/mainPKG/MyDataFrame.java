a4b28edd0b54b28aaac428adb71e2856b90612cd
sarahpg <sarahpg@umich.edu>
2017-11-28 11:11:51 -0600

    Restructuring and adding many methods

--- src/mainPKG/MyDataFrame.java
+++ src/mainPKG/MyDataFrame.java
@@ -1,107 +1,254 @@
 package mainPKG;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+
 
 public class MyDataFrame {
-    private ArrayList<String> state; 
-    private ArrayList<String> gender;
-    private ArrayList<Integer> year;
-    private ArrayList<String> name;
-    private ArrayList<Integer> count;
     
-    public MyDataFrame(ArrayList<String> state, ArrayList<String> gender, ArrayList<Integer> year, 
-    		ArrayList<String> name, ArrayList<Integer> count) {
-    		this.state = state;
-    		this.gender = gender;
-    		this.year = year;
-    		this.name = name;
-    		this.count = count;  	
+    private ArrayList<Object> col0; 
+    private ArrayList<Object> col1;
+    private ArrayList<Object> col2;
+    private ArrayList<Object> col3;
+    private ArrayList<Object> col4;
+    HashMap<String,Integer> colNames = new HashMap<String,Integer>();
+    
+    public MyDataFrame() {
+    		this.col0 = null;
+    		this.col1 = null;
+    		this.col2 = null;
+    		this.col3 = null;
+    		this.col4 = null; 
+    		this.colNames = null;
     }
-    public ArrayList<String> getState() {
-		return(this.state);
+    
+    public MyDataFrame(ArrayList<Object> col0, ArrayList<Object> col1, ArrayList<Object> col2, 
+    		ArrayList<Object> col3, ArrayList<Object> col4, HashMap<String,Integer> colNames) {
+    		this.col0 = col0;
+    		this.col1 = col1;
+    		this.col2 = col2;
+    		this.col3 = col3;
+    		this.col4 = col4; 
+    		this.colNames = colNames;
     }
-    public ArrayList<String> getGender() {
-		return(this.gender);
+    public ArrayList<Object> getCol0() {
+		return(this.col0);
     }
-    public ArrayList<Integer> getYear() {
-		return(this.year);
+    public ArrayList<Object> getCol1() {
+		return(this.col1);
     }
-    public ArrayList<String> getName() {
-    		return(this.name);
+    public ArrayList<Object> getCol2() {
+		return(this.col2);
     }
-    public ArrayList<Integer> getCount() {
-    		return(this.count);
+    public ArrayList<Object> getCol3() {
+		return(this.col3);
     }
-
-	
-	/*public MyDataFrame head(int n) {
-		return rows;
+    public ArrayList<Object> getCol4() {
+		return(this.col4);
+    }
+    public HashMap<String,Integer> getColNames() {
+		return(this.colNames);
+    }
+    
+    private void setCol0(ArrayList<Object> col) {
+    		this.col0 = col;
+    }
+    private void setCol1(ArrayList<Object> col) {
+		this.col1 = col;
+    }
+    private void setCol2(ArrayList<Object> col) {
+		this.col2 = col;
+    }
+    private void setCol3(ArrayList<Object> col) {
+		this.col3 = col;
+    }
+    private void setCol4(ArrayList<Object> col) {
+		this.col4 = col;
+    }
+    
+	public MyDataFrame head(int n) {
+		ArrayList<Object> col0 = new ArrayList<Object>(this.col0.subList(0, n));
+		ArrayList<Object> col1 = new ArrayList<Object>(this.col1.subList(0, n));
+		ArrayList<Object> col2 = new ArrayList<Object>(this.col2.subList(0, n));
+		ArrayList<Object> col3 = new ArrayList<Object>(this.col3.subList(0, n));
+		ArrayList<Object> col4 = new ArrayList<Object>(this.col4.subList(0, n));
+	    
+		return new MyDataFrame(col0, col1, col2, col3, col4, this.getColNames());
 	}
 	
 	public MyDataFrame tail(int n) {
-		return rows;
+		ArrayList<Object> col0 = new ArrayList<Object>(this.col0.subList(this.col0.size()-n, this.col0.size()));
+		ArrayList<Object> col1 = new ArrayList<Object>(this.col1.subList(this.col1.size()-n, this.col1.size()));
+		ArrayList<Object> col2 = new ArrayList<Object>(this.col2.subList(this.col2.size()-n, this.col2.size()));
+		ArrayList<Object> col3 = new ArrayList<Object>(this.col3.subList(this.col3.size()-n, this.col3.size()));
+		ArrayList<Object> col4 = new ArrayList<Object>(this.col4.subList(this.col4.size()-n, this.col4.size()));
+		
+		return new MyDataFrame(col0, col1, col2, col3, col4, this.getColNames());
 	}
-	
+
+	/*
 	public String dType(int index) {
-		return type;
+		String[] columns = {"String", "String", "Integer", "String", "Integer"};
+		if(index > 5 | index < 0 ) {
+			return "Not a valid index.";
+		}
+		return columns[index];
 	}
 	
+
 	public String dType(String name) {
-		return type;
-	}
-	
+		Hashtable<String, String> hashtable = new Hashtable<String, String>();
+		hashtable.put("state","String");
+		hashtable.put("gender","String");
+		hashtable.put("name","String");
+		hashtable.put("year","Integer");
+		hashtable.put("count","Integer");
+		if(!hashtable.containsKey(name)) {
+			return "Not a valid index.";
+		}
+		return hashtable.get(name);
+	}*/
+
 	public MyDataFrame slice(int index) {
-		return column;
-	}
-	
+		MyDataFrame result = new MyDataFrame();
+		if(index == 0) {
+			result.setCol0(this.col0);
+		}
+		if(index == 1) {
+			result.setCol1(this.col1);
+		}
+		if(index == 2) {
+			result.setCol2(this.col2);
+		}
+		if(index == 3) {
+			result.setCol3(this.col3);
+		}
+		if(index == 4) {
+			result.setCol4(this.col4);
+		}
+		return result;
+	}	
+		
 	public MyDataFrame slice(String name) {
-		return column;
+		MyDataFrame result = new MyDataFrame();
+		int index = this.getColNames().get(name);
+		if(index == 0) {
+			result.setCol0(this.col0);
+		}
+		if(index == 1) {
+			result.setCol1(this.col1);
+		}
+		if(index == 2) {
+			result.setCol2(this.col2);
+		}
+		if(index == 3) {
+			result.setCol3(this.col3);
+		}
+		if(index == 4) {
+			result.setCol4(this.col4);
+		}
+		return result;
 	}
 	
 	public MyDataFrame slice(int[] indexArr) {
-		return columns;
+		MyDataFrame result = new MyDataFrame();
+		for(int index: indexArr) {
+			if(index == 0) {
+				result.setCol0(this.col0);
+			}
+			if(index == 1) {
+				result.setCol1(this.col1);
+			}
+			if(index == 2) {
+				result.setCol2(this.col2);
+			}
+			if(index == 3) {
+				result.setCol3(this.col3);
+			}
+			if(index == 4) {
+				result.setCol4(this.col4);
+			}
+		}
+		return result;
 	}
-	
-	public MyDataFrame filter(String col, String op, Object o) {
-		return filteredData;
-	}
-	
+
+	public MyDataFrame slice(String[] nameArr) {
+		MyDataFrame result = new MyDataFrame();
+		for(String index: nameArr) {
+			if(this.getColNames().get(index) == 0) {
+				result.setCol0(this.col0);
+			}
+			if(this.getColNames().get(index) == 1) {
+				result.setCol1(this.col1);
+			}
+			if(this.getColNames().get(index) == 2) {
+				result.setCol2(this.col2);
+			}
+			if(this.getColNames().get(index) == 3) {
+				result.setCol3(this.col3);
+			}
+			if(this.getColNames().get(index) == 4) {
+				result.setCol4(this.col4);
+			}
+		}
+	return result;
+}
+//	
+//	public MyDataFrame filter(String col, String op, Object o) {
+//		return filteredData;
+//	}
+//	
 	public MyDataFrame loc(int index) {
-		return rows;
-	}
-	
-	public MyDataFrame loc(String label) {
-		return rows;
+	    ArrayList<Object> col0 = new ArrayList<Object>();
+	    ArrayList<Object> col1 = new ArrayList<Object> ();
+	    ArrayList<Object> col2 = new ArrayList<Object> ();
+	    ArrayList<Object> col3 = new ArrayList<Object> ();
+	    ArrayList<Object> col4 = new ArrayList<Object> ();
+	    
+	    
+	    col0.add(this.col0.get(index));
+	    col1.add(this.col1.get(index));
+	    col2.add(this.col2.get(index));
+	    col3.add(this.col3.get(index));
+	    col4.add(this.col4.get(index));
+
+	    return(new MyDataFrame(col0, col1, col2, col3, col4, this.getColNames()));
 	}
 	
 	public MyDataFrame loc(int from, int to) {
-		return rows;
+		ArrayList<Object> col0 = new ArrayList<Object>(this.col0.subList(from, to));
+		ArrayList<Object> col1 = new ArrayList<Object>(this.col1.subList(from, to));
+		ArrayList<Object> col2 = new ArrayList<Object>(this.col2.subList(from, to));
+		ArrayList<Object> col3 = new ArrayList<Object>(this.col3.subList(from, to));
+		ArrayList<Object> col4 = new ArrayList<Object>(this.col4.subList(from, to));
+	    
+		return new MyDataFrame(col0, col1, col2, col3, col4, this.getColNames());
 	}
+
+//	
+//	public MyDataFrame sort(int index) {
+//		return sortedData;
+//	}
+//	
+//	public MyDataFrame sort(String name) {
+//		return sortedData;
+//	}
+//	
+//	public Object getMin(int index) {
+//		Object min; 
+//		return min;
+//	}
+//	
+//	public Object getMin(String label) {
+//		return min;
+//	}
+//	
+//	public Object getMax(int index) {
+//		return max;
+//	}
+//	
+//	public Object getMax(String label) {
+//		return max;
+//	} 
 	
-	public MyDataFrame loc(String from, String to) {
-		return rows;
-	}
-	
-	public MyDataFrame sort(int index) {
-		return sortedData;
-	}
-	
-	public MyDataFrame sort(String name) {
-		return sortedData;
-	}
-	
-	public Object getMin(int index) {
-		return min;
-	}
-	
-	public Object getMin(String label) {
-		return min;
-	}
-	
-	public Object getMax(int index) {
-		return max;
-	}
-	
-	public Object getMax(String label) {
-		return max;
-	} */
 }
--- src/mainPKG/MyPandas.java
+++ src/mainPKG/MyPandas.java
@@ -1,39 +1,66 @@
 package mainPKG;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 
 public class MyPandas {
-
     public MyDataFrame readCSV(String path) throws FileNotFoundException {
-    		Scanner scan = new Scanner(new File(path));
-	    ArrayList<String> state = new ArrayList<String> ();
-	    ArrayList<String> gender = new ArrayList<String> ();
-	    ArrayList<String> name = new ArrayList<String> ();
-	    ArrayList<Integer> year = new ArrayList<Integer> ();
-	    ArrayList<Integer> count = new ArrayList<Integer> ();
-	    
-	    scan.nextLine(); // skip the first line with headers
-	    while(scan.hasNextLine()){
-	    		String temp[]=scan.nextLine().split(",");
-	    		state.add(temp[1]);
-	    		gender.add(temp[2]);
-	    		year.add(Integer.parseInt(temp[3]));
-	    		name.add(temp[4]);
-	    		count.add(Integer.parseInt(temp[5]));
-	    		}
-	    scan.close();
-	    return(new MyDataFrame(state, gender, year, name, count));
+		Scanner scan = new Scanner(new File(path));
+    ArrayList<Object> state = new ArrayList<Object> ();
+    ArrayList<Object> gender = new ArrayList<Object> ();
+    ArrayList<Object> name = new ArrayList<Object> ();
+    ArrayList<Object> year = new ArrayList<Object>();
+    ArrayList<Object> count = new ArrayList<Object> ();
+    HashMap<String,Integer> colNames = new HashMap<String,Integer> ();
+    
+    String names[]=scan.nextLine().split(",");
+    for(int i = 0; i < names.length; i++) {
+    		colNames.put(names[i].replaceAll("^\"|\"$", ""), i);
     }
-    /*
-    public void writeCSV(MyDataFrame data, String path) {
-        
+
+    while(scan.hasNextLine()){
+    		String temp[]=scan.nextLine().split(",");
+    		state.add(temp[0]);
+    		gender.add(temp[1]);
+    		year.add(Integer.parseInt(temp[2]));
+    		name.add(temp[3]);
+    		count.add(Integer.parseInt(temp[4]));
+    		}
+    scan.close();
+    return(new MyDataFrame(state, gender, year, name, count, colNames));
+}
+
+    public void writeCSV(MyDataFrame data, String path) throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter(path, "UTF-8");
+		for(int i = 0; i < data.getCol1().size(); i++) {
+			writer.println(data.getCol0().get(i) + "," + data.getCol1().get(i) + "," +
+					data.getCol2().get(i) + "," + data.getCol3().get(i) + "," + 
+					data.getCol4().get(i));
+		}
+		writer.close();
     }
     
     public MyDataFrame concat(MyDataFrame df1, MyDataFrame df2) {
-        return type; 
-    }*/
-
+    		ArrayList<Object> col0 = new ArrayList<Object>(df1.getCol0());	
+    		ArrayList<Object> col1 = new ArrayList<Object>(df1.getCol1());
+    		ArrayList<Object> col2 = new ArrayList<Object>(df1.getCol2());
+    		ArrayList<Object> col3 = new ArrayList<Object>(df1.getCol3());
+    		ArrayList<Object> col4 = new ArrayList<Object>(df1.getCol4());
+    		
+	    
+    		MyDataFrame result = new MyDataFrame(col0, col1, col2, col3, col4, df1.getColNames());
+    		
+    		result.getCol0().addAll(df2.getCol0());
+    		result.getCol1().addAll(df2.getCol1());
+    		result.getCol2().addAll(df2.getCol2());
+    		result.getCol3().addAll(df2.getCol3());
+    		result.getCol4().addAll(df2.getCol4());
+    		
+    		return result;
+    }
 }
